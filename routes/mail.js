f6f66d02b3395c0379ee23ac2b3c0c747a897dad
AS <ahmed.sayed.fcis1997@gmail.com>
2022-07-01 02:09:16 +0200

    add validation function

--- routes/mail.js
+++ routes/mail.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const sendMail = require("./../services/sendgrid.service")
 
-router.post('/', (req, res) => {
-  sendMail("ahmed.sayed.fcis1997@gmail.com")
-  res.send({
-    message: "Okay"
-  })
+router.post('/', async (req, res) => {
+  try {
+    const rr = await sendMail({
+      to: "ahmed.sayed.fcis1997@gmail.com",
+      from: "ahmed.sayed.fcis1997@gmail.com",
+      subject: "test",
+      text: "hi"
+    })
+    res.send({
+      message: "Okay"
+    })
+  } catch (error) {
+    
+  }
+
 });
 
 module.exports = router;
--- services/sendgrid.service.js
+++ services/sendgrid.service.js
@@ -1,6 +1,7 @@
+const { DEBUG_SERVER } = require("../constant")
 const sgMail = require("@sendgrid/mail");
-const debug
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const debug = require("debug")(DEBUG_SERVER);
 
 const send = async ({ to, from, subject, text, html }) => {
   try {
@@ -12,8 +13,10 @@ const send = async ({ to, from, subject, text, html }) => {
       html,
     };
     const result = await sgMail.send(msg);
+    debug({statusCode: result[0].statusCode})
+    debug({headers: result[0].headers})
   } catch (error) {
-    console.error({error})
+    debug({error})
   }
 
 };
